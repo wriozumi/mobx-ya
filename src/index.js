dcf28dee280966e64d4d4e1e0ecec69d7b26e633
wriozumi <tshatmanaliev@gmail.com>
2019-08-03 08:04:08 +0600

    Observable Object

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import DevTools from "mobx-react-devtools";
+import { observable, computed } from "mobx";
+import { observer } from "mobx-react";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const nickName = observable({
+  firstName: "Wrio",
+  age: 30,
+
+  get nickName() {
+    console.log(`Generate nickname!`);
+    return `${this.firstName}${this.age}`;
+  },
+  increment() {
+    this.age++;
+  },
+  decrement() {
+    this.age--;
+  }
+});
+@observer
+class Counter extends React.Component {
+  handleDecrement = () => {
+    this.props.store.decrement();
+  };
+  handleIncrement = () => {
+    this.props.store.increment();
+  };
+  render() {
+    return (
+      <div className="App">
+        {/* <DevTools /> */}
+        <h1>{this.props.store.nickName}</h1>
+        <h1>{this.props.store.age}</h1>
+        <button onClick={this.handleDecrement}>-1</button>
+        <button onClick={this.handleIncrement}>+1</button>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<Counter store={nickName} />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
