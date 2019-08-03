b48330e7f29badc28682a6a75b252001927ffee0
wriozumi <tshatmanaliev@gmail.com>
2019-08-03 12:23:19 +0600

    Sprint board app

--- src/index.js
+++ src/index.js
@@ -1,60 +1,137 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import * as serviceWorker from "./serviceWorker";
-import DevTools from "mobx-react-devtools";
-import { observable, computed, configure, action } from "mobx";
-import { observer } from "mobx-react";
+import './index.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import { observable, computed, configure, action, decorate } from 'mobx';
+import { observer } from 'mobx-react';
+import DevTools from 'mobx-react-devtools';
+configure({ enforceActions: 'observed' });
 
-configure({ enforceActions: "observed" });
+class Store {
+  devsList = [
+    { name: "Jack", sp: 12 },
+    { name: "Max", sp: 10 },
+		{ name: "Leo", sp: 8 },
+  ];
+  filter: '';
 
-const nickName = observable(
-  {
-    firstName: "Wrio",
-    age: 30,
+	get totalSum() {
+    return this.devsList.reduce((sum, { sp }) => sum += sp, 0);
+  };
 
-    get nickName() {
-      console.log(`Generate nickname!`);
-      return `${this.firstName}${this.age}`;
-    },
-    increment() {
-      this.age++;
-    },
-    decrement() {
-      this.age--;
-    }
-  },
-  {
-    increment: action,
-    decrement: action
+  get topPerformer() {
+    const maxSp = Math.max(...this.devsList.map(({ sp }) => sp));
+    return this.devsList.find(({ sp, name }) => {
+      if(maxSp === sp) {
+        return name;
+      }
+    });
+  };
+
+  get filteredDevelopers() {
+    const matchesFilter = new RegExp(this.filter, "i");
+    return this.devsList.filter(({ name }) => !this.filter || matchesFilter.test(name));
   }
-);
 
-const todos = observable([{ text: "Learn React" }, { text: "Learn MobX" }]);
-@observer
-class Counter extends React.Component {
-  handleDecrement = () => {
-    this.props.store.decrement();
+  clearList() {
+    this.devsList = [];
   };
-  handleIncrement = () => {
-    this.props.store.increment();
+
+  addDeveloper(dev) {
+    this.devsList.push(dev);
   };
+
+  updateFilter(value) {
+    this.filter = value;
+  }
+};
+
+decorate(Store, {
+  devsList: observable,
+  filter: observable,
+  totalSum: computed,
+  topPerformer: computed,
+  filteredDevelopers: computed,
+  clearList: action,
+  addDeveloper: action,
+  updateFilter: action,
+})
+
+const appStore = new Store();
+
+const Row = ({ data: { name, sp } }) => {
+  return (
+		<tr>
+    	<td>{name}</td>
+    	<td>{sp}</td>
+  	</tr>
+	);
+};
+
+@observer class Table extends Component {
   render() {
+    const { store } = this.props;
+
     return (
-      <div className="App">
-        {/* <DevTools /> */}
-        <h1>{this.props.store.nickName}</h1>
-        <h1>{this.props.store.age}</h1>
-        <button onClick={this.handleDecrement}>-1</button>
-        <button onClick={this.handleIncrement}>+1</button>
-      </div>
-    );
+      <table>
+        <thead>
+          <tr>
+            <td>Name:</td>
+            <td>SP:</td>
+          </tr>
+        </thead>
+        <tbody>
+          {store.filteredDevelopers.map((dev, i) => <Row key={i} data={dev} />)}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>Team SP:</td>
+            <td>{store.totalSum}</td>
+          </tr>
+          <tr>
+            <td>Top Performer:</td>
+            <td>{store.topPerformer ? store.topPerformer.name : ''}</td>
+          </tr>
+        </tfoot>
+      </table>
+		);
   }
 }
 
-ReactDOM.render(<Counter store={nickName} />, document.getElementById("root"));
+@observer class Controls extends Component {
+  addDeveloper = () => {
+    const name = prompt("The name:");
+    const sp = parseInt(prompt("The story points:"), 10);
+    this.props.store.addDeveloper({ name, sp });
+  }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  clearList = () => { this.props.store.clearList(); }
+
+  filterDevelopers = ({ target: { value } }) => {
+    this.props.store.updateFilter(value);
+  }
+
+  render() {
+    return (
+			<div className="controls">
+      	<button onClick={this.clearList}>Clear table</button>
+      	<button onClick={this.addDeveloper}>Add record</button>
+        <input value={this.props.store.filter} onChange={this.filterDevelopers} />
+    	</div>
+		);
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+				{/* <DevTools /> */}
+        <h1>Sprint Board:</h1>
+        <Controls store={appStore} />
+        <Table store={appStore} />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<App store={Store} />, document.getElementById('root'));
